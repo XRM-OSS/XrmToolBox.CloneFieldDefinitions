058cac8dee873c7b65193feea503ec5836a27a1d
piyush <ppaliwal.89@gmail.com>
2019-06-18 17:15:41 +0530

    Revert "Added "Clear Selection" button to easily uncheck the checkboxes"
    This reverts commit dde31d937b33f00c6421a6bf94a5db291bf4fffc.

--- src/lib/MsDyn.Contrib.CloneFieldDefinitions/CloneFieldsControl.cs
+++ src/lib/MsDyn.Contrib.CloneFieldDefinitions/CloneFieldsControl.cs
@@ -40,7 +40,6 @@ namespace MsDyn.Contrib.CloneFieldDefinitions
         private Button button2;
         private readonly List<EntityMetadata> _entitiesDetailedSource = new List<EntityMetadata>();
         private Button button3;
-        private Button button4;
         private List<EntityMetadata> _entitiesDetailedTarget = new List<EntityMetadata>();
 
         public string RepositoryName => "XrmToolBox.CloneFieldDefinitions";
@@ -171,14 +170,13 @@ namespace MsDyn.Contrib.CloneFieldDefinitions
             this.prefixOverrideLabel = new System.Windows.Forms.Label();
             this.prefixOverride = new System.Windows.Forms.CheckBox();
             this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
             this.listView1 = new System.Windows.Forms.ListView();
             this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.flowLayoutPanel2.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -197,20 +195,17 @@ namespace MsDyn.Contrib.CloneFieldDefinitions
             this.flowLayoutPanel2.Controls.Add(this.prefixOverride);
             this.flowLayoutPanel2.Controls.Add(this.button2);
             this.flowLayoutPanel2.Controls.Add(this.button3);
-            this.flowLayoutPanel2.Controls.Add(this.button4);
-            this.flowLayoutPanel2.Location = new System.Drawing.Point(8, 7);
-            this.flowLayoutPanel2.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(3, 3);
             this.flowLayoutPanel2.Name = "flowLayoutPanel2";
-            this.flowLayoutPanel2.Size = new System.Drawing.Size(3278, 131);
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(1129, 39);
             this.flowLayoutPanel2.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(8, 10);
-            this.label1.Margin = new System.Windows.Forms.Padding(8, 10, 8, 10);
+            this.label1.Location = new System.Drawing.Point(3, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(184, 32);
+            this.label1.Size = new System.Drawing.Size(70, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "Source Entity";
             // 
@@ -218,21 +213,19 @@ namespace MsDyn.Contrib.CloneFieldDefinitions
             // 
             this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(208, 7);
-            this.comboBox1.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.comboBox1.Location = new System.Drawing.Point(79, 3);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(316, 39);
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
             this.comboBox1.TabIndex = 1;
-            this.comboBox1.DropDown += new System.EventHandler(this.AdjustWidthComboBox_DropDown);
             this.comboBox1.SelectedValueChanged += new System.EventHandler(this.OnSelectSourceEntity);
+            this.comboBox1.DropDown += new System.EventHandler(this.AdjustWidthComboBox_DropDown);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(540, 10);
-            this.label2.Margin = new System.Windows.Forms.Padding(8, 10, 8, 10);
+            this.label2.Location = new System.Drawing.Point(206, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(176, 32);
+            this.label2.Size = new System.Drawing.Size(67, 13);
             this.label2.TabIndex = 2;
             this.label2.Text = "Target Entity";
             // 
@@ -240,21 +233,19 @@ namespace MsDyn.Contrib.CloneFieldDefinitions
             // 
             this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(732, 7);
-            this.comboBox2.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.comboBox2.Location = new System.Drawing.Point(279, 3);
             this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(316, 39);
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
             this.comboBox2.TabIndex = 3;
-            this.comboBox2.DropDown += new System.EventHandler(this.AdjustWidthComboBox_DropDown);
             this.comboBox2.SelectedValueChanged += new System.EventHandler(this.OnSelectTargetEntity);
+            this.comboBox2.DropDown += new System.EventHandler(this.AdjustWidthComboBox_DropDown);
             // 
             // button1
             // 
             this.button1.Dock = System.Windows.Forms.DockStyle.Right;
-            this.button1.Location = new System.Drawing.Point(1064, 7);
-            this.button1.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.button1.Location = new System.Drawing.Point(406, 3);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(363, 55);
+            this.button1.Size = new System.Drawing.Size(136, 24);
             this.button1.TabIndex = 4;
             this.button1.Text = "Clone selected Fields";
             this.button1.UseVisualStyleBackColor = true;
@@ -263,62 +254,45 @@ namespace MsDyn.Contrib.CloneFieldDefinitions
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(1443, 10);
-            this.label3.Margin = new System.Windows.Forms.Padding(8, 10, 8, 10);
+            this.label3.Location = new System.Drawing.Point(548, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(158, 32);
+            this.label3.Size = new System.Drawing.Size(58, 13);
             this.label3.TabIndex = 6;
             this.label3.Text = "Field Prefix";
             this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // txtPrefix
             // 
-            this.txtPrefix.Location = new System.Drawing.Point(1617, 7);
-            this.txtPrefix.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.txtPrefix.Location = new System.Drawing.Point(612, 3);
             this.txtPrefix.Name = "txtPrefix";
-            this.txtPrefix.Size = new System.Drawing.Size(260, 38);
+            this.txtPrefix.Size = new System.Drawing.Size(100, 20);
             this.txtPrefix.TabIndex = 5;
             // 
             // prefixOverrideLabel
             // 
             this.prefixOverrideLabel.AutoSize = true;
-            this.prefixOverrideLabel.Location = new System.Drawing.Point(1893, 10);
-            this.prefixOverrideLabel.Margin = new System.Windows.Forms.Padding(8, 10, 8, 10);
+            this.prefixOverrideLabel.Location = new System.Drawing.Point(718, 0);
             this.prefixOverrideLabel.Name = "prefixOverrideLabel";
-            this.prefixOverrideLabel.Size = new System.Drawing.Size(204, 32);
+            this.prefixOverrideLabel.Size = new System.Drawing.Size(76, 13);
             this.prefixOverrideLabel.TabIndex = 7;
             this.prefixOverrideLabel.Text = "Override Prefix";
             // 
             // prefixOverride
             // 
-            this.prefixOverride.AutoSize = true;
-            this.prefixOverride.Location = new System.Drawing.Point(2113, 10);
-            this.prefixOverride.Margin = new System.Windows.Forms.Padding(8, 10, 8, 10);
+            this.prefixOverride.Location = new System.Drawing.Point(800, 3);
             this.prefixOverride.Name = "prefixOverride";
-            this.prefixOverride.Size = new System.Drawing.Size(34, 33);
+            this.prefixOverride.Size = new System.Drawing.Size(18, 24);
             this.prefixOverride.TabIndex = 8;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(2163, 7);
-            this.button2.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.button2.Location = new System.Drawing.Point(824, 3);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(509, 55);
+            this.button2.Size = new System.Drawing.Size(191, 23);
             this.button2.TabIndex = 9;
             this.button2.Text = "Target Org: Source";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(2688, 7);
-            this.button3.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(259, 55);
-            this.button3.TabIndex = 10;
-            this.button3.Text = "Reset Target Org";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // listView1
             // 
@@ -336,12 +310,10 @@ namespace MsDyn.Contrib.CloneFieldDefinitions
             this.columnHeader4,
             this.columnHeader5});
             this.listView1.FullRowSelect = true;
-            this.listView1.HideSelection = false;
             this.listView1.ImeMode = System.Windows.Forms.ImeMode.On;
-            this.listView1.Location = new System.Drawing.Point(8, 152);
-            this.listView1.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.listView1.Location = new System.Drawing.Point(3, 48);
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(3271, 1013);
+            this.listView1.Size = new System.Drawing.Size(1129, 443);
             this.listView1.Sorting = System.Windows.Forms.SortOrder.Ascending;
             this.listView1.TabIndex = 1;
             this.listView1.UseCompatibleStateImageBehavior = false;
@@ -372,27 +344,25 @@ namespace MsDyn.Contrib.CloneFieldDefinitions
             this.columnHeader5.Text = "Custom";
             this.columnHeader5.Width = 150;
             // 
-            // button4
+            // button3
             // 
-            this.button4.Location = new System.Drawing.Point(2963, 7);
-            this.button4.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(228, 55);
-            this.button4.TabIndex = 11;
-            this.button4.Text = "Clear Selection";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
+            this.button3.Location = new System.Drawing.Point(1021, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(97, 23);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Reset Target Org";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // CloneFieldDefinitionsControl
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
             this.Controls.Add(this.flowLayoutPanel2);
             this.Controls.Add(this.listView1);
-            this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.Name = "CloneFieldDefinitionsControl";
-            this.Size = new System.Drawing.Size(3310, 1192);
+            this.Size = new System.Drawing.Size(1141, 500);
             this.flowLayoutPanel2.ResumeLayout(false);
             this.flowLayoutPanel2.PerformLayout();
             this.ResumeLayout(false);
@@ -801,14 +771,6 @@ namespace MsDyn.Contrib.CloneFieldDefinitions
             RemoveAdditionalOrganization(AdditionalConnectionDetails[0]);
             button2.Text = "Target Org: Source";
             RetrieveAvailableEntities(null, null);
-        }
-
-        private void button4_Click(object sender, EventArgs e)
-        {
-            foreach (ListViewItem item in listView1.CheckedItems)
-            {
-                item.Checked = false;
-            }
         }
     }
 }
