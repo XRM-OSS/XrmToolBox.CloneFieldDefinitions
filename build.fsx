fec42b26a1872adfe46e2e8d0b4dacee3a3fd08d
Florian Krönert <DigitalFlow@users.noreply.github.com>
2019-10-07 08:42:54 +0200

    Update build.fsx

--- build.fsx
+++ build.fsx
@@ -25,7 +25,7 @@ let packagesDir = @".\packages\"
 // version info
 let majorversion    = 1
 let minorversion    = 2
-let patch           = 0
+let patch           = 1
 
 let sha                     = Git.Information.getCurrentHash() 
 
